c4030b9ee9086c01d3f20817fcadb091d620a524
cmoynihan70 <58593460+cmoynihan70@users.noreply.github.com>
2023-02-09 13:51:48 +0000

    Some minor changes

--- steps/08_orchestrate_jobs.sql
+++ steps/08_orchestrate_jobs.sql
@@ -40,6 +40,7 @@ CALL ANALYTICS.DAILY_CITY_METRICS_UPDATE_SP();
 ALTER TASK DAILY_CITY_METRICS_UPDATE_TASK RESUME;
 EXECUTE TASK ORDERS_UPDATE_TASK;
 
+SHOW TASKS;
 
 -- ----------------------------------------------------------------------------
 -- Step #3: Monitor tasks in Snowsight
